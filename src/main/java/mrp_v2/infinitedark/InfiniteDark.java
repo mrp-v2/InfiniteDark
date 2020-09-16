25f786c47558f9cdd4a6c71a939b4a152a4b15b4
mrp-v2 <matthrewp@gmail.com>
2020-09-16 08:55:36 -0400

    created mod

---
+++ src/main/java/mrp_v2/infinitedark/InfiniteDark.java
@@ -0,0 +1,8 @@
+package mrp_v2.infinitedark;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(InfiniteDark.ID) public class InfiniteDark
+{
+    public static final String ID = "infinite" + "dark";
+}
