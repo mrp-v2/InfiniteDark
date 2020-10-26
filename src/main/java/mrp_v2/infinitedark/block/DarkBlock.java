ad9223d03ceed0431f2436c39b0992fddf6951e9
mrp-v2 <matthrewp@gmail.com>
2020-10-26 07:33:22 -0400

    remove unnecessary event bus subscriber annotation

--- src/main/java/mrp_v2/infinitedark/block/DarkBlock.java
+++ src/main/java/mrp_v2/infinitedark/block/DarkBlock.java
@@ -1,13 +1,11 @@
 package mrp_v2.infinitedark.block;
 
-import mrp_v2.infinitedark.InfiniteDark;
 import mrp_v2.infinitedark.util.Util;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(modid = InfiniteDark.ID) public class DarkBlock extends Block
+public class DarkBlock extends Block
 {
     public static final Block PARENT_BLOCK = Blocks.BLACK_CONCRETE;
     public static final ResourceLocation ID = Util.makeLoc("dark");
